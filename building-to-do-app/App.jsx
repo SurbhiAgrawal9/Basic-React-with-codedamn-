8b57bc111bf5d2907a5fec01f1238dcfcdb3e3e8
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-05-30 22:36:20 +0530

    Create App.jsx

---
+++ building-to-do-app/App.jsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+import {useState} from 'react'
+
+
+function App(){
+  const [task, setTask] = useState("")
+  const [todos,setTodo] = useState(["tony","marvel","ironmen","tom"])
+function createTodo(){
+    setTodo(oldTodos => {
+        return [...oldTodos, task]
+    })
+//  console.log(task)
+}
+return(
+   <div className="App" >
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={createTodo}>add todo</button> 
+      <ul>
+       {
+         todos.map((item) => {
+           return <li>{item}</li>
+         })
+     }
+     </ul>
+     </div>
+   );
+}
+export default App
